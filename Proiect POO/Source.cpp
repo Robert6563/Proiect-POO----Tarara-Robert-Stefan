fba03e60a52260a9b6ea785a03c7a9a8ea2d86ed
Robert6563 <120989323+Robert6563@users.noreply.github.com>
2023-01-15 22:16:48 +0200

    Update Source.cpp

--- Proiect POO/Source.cpp
+++ Proiect POO/Source.cpp
@@ -1,112 +1,77 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
-class Locatie {
+class Zona {
 private:
 	string denumire;
-	string* zone;
-	int nrZone;
-	int nrMaxLocuri;
 	bool* randuri;
 	int nrRanduri;
 	bool** scaune;
 	int nrScaunePeRand;
-	string adresa;
-	string localitate;
-	static string tara;
+	int pret;
 
 public:
-	Locatie()
+	Zona()
 	{
 		denumire = "Necunoscuta";
-		zone = nullptr;
-		nrZone = 0;
-		nrMaxLocuri = 0;
 		randuri = nullptr;
 		nrRanduri = 0;
 		scaune = nullptr;
 		nrScaunePeRand = 0;
-		adresa = "Necunoscuta";
-		localitate = "Necunoscuta";
+		pret = 0;
 	}
 
-	Locatie(string denumire, string* zone, int nrZone, int nrMaxLocuri, int* randuri, int nrRanduri, int** scaune, int nrScaunePeRand, string adresa, string localitate)
+	Zona(string denumire, int nrRanduri, int nrScaunePeRand)
 	{
 		this->denumire = denumire;
-		this->zone = new string[nrZone];
-		for (int i = 0; i < nrZone; i++)
-			this->zone[i] = zone[i];
-		this->nrZone = nrZone;
-		this->nrMaxLocuri = nrMaxLocuri;
-		this->randuri = new bool[nrRanduri];
-		for (int i = 0; i < nrRanduri; i++)
-			this->randuri[i] = randuri[i];
 		this->nrRanduri = nrRanduri;
-		for (int i = 0; i < nrRanduri; i++)
-			this->scaune[i] = new bool[nrScaunePeRand];
-		for (int i = 0; i < nrRanduri; i++)
-			for (int j = 0; j < nrScaunePeRand; j++)
-				this->scaune[i][j] = scaune[i][j];
 		this->nrScaunePeRand = nrScaunePeRand;
-		this->adresa = adresa;
-		this->localitate = localitate;
+		this->pret = pret;
 	}
 
-	~Locatie()
+	~Zona()
 	{
-		if (nrZone > 0)
-			delete[] zone;
 		if (nrRanduri > 0)
 			delete[] randuri;
-		if (scaune != nullptr)
-			delete[] scaune;
+		for (int i = 0; i < nrRanduri; i++)
+			if (scaune[i] != nullptr)
+				delete[] scaune[i];
 	}
 
-	Locatie(const Locatie& l)
+	Zona(const Zona& z)
 	{
-		this->denumire = l.denumire;
-		this->zone = new string[l.nrZone];
-		for (int i = 0; i < l.nrZone; i++)
-			this->zone[i] = l.zone[i];
-		this->nrZone = l.nrZone;
-		this->nrMaxLocuri = l.nrMaxLocuri;
-		this->randuri = new bool[l.nrRanduri];
-		for (int i = 0; i < l.nrRanduri; i++)
-			this->randuri[i] = l.randuri[i];
-		this->nrRanduri = l.nrRanduri;
-		for (int i = 0; i < l.nrRanduri; i++)
-			this->scaune[i] = new bool[l.nrScaunePeRand];
-		for (int i = 0; i < l.nrRanduri; i++)
-			for (int j = 0; j < l.nrScaunePeRand; j++)
-				this->scaune[i][j] = l.scaune[i][j];
-		this->nrScaunePeRand = l.nrScaunePeRand;
-		this->adresa = l.adresa;
-		this->localitate = l.localitate;
+		this->denumire = z.denumire;
+		this->randuri = new bool[z.nrRanduri];
+		for (int i = 0; i < z.nrRanduri; i++)
+			this->randuri[i] = z.randuri[i];
+		this->nrRanduri = z.nrRanduri;
+		for (int i = 0; i < z.nrRanduri; i++)
+			this->scaune[i] = new bool[z.nrScaunePeRand];
+		for (int i = 0; i < z.nrRanduri; i++)
+			for (int j = 0; j < z.nrScaunePeRand; j++)
+				this->scaune[i][j] = z.scaune[i][j];
+		this->nrScaunePeRand = z.nrScaunePeRand;
+		this->pret = z.pret;
 	}
 
-	Locatie operator=(Locatie& l)
+	Zona operator=(Zona& z)
 	{
-		if (this != &l)
+		if (this != &z)
 		{
-			this->denumire = l.denumire;
-			this->zone = new string[l.nrZone];
-			for (int i = 0; i < l.nrZone; i++)
-				this->zone[i] = l.zone[i];
-			this->nrZone = l.nrZone;
-			this->nrMaxLocuri = l.nrMaxLocuri;
-			this->randuri = new bool[l.nrRanduri];
-			for (int i = 0; i < l.nrRanduri; i++)
-				this->randuri[i] = l.randuri[i];
-			this->nrRanduri = l.nrRanduri;
-			for (int i = 0; i < l.nrRanduri; i++)
-				this->scaune[i] = new bool[l.nrScaunePeRand];
-			for (int i = 0; i < l.nrRanduri; i++)
-				for (int j = 0; j < l.nrScaunePeRand; j++)
-					this->scaune[i][j] = l.scaune[i][j];
-			this->nrScaunePeRand = l.nrScaunePeRand;
-			this->adresa = l.adresa;
-			this->localitate = l.localitate;
+			this->denumire = z.denumire;
+			this->randuri = new bool[z.nrRanduri];
+			for (int i = 0; i < z.nrRanduri; i++)
+				this->randuri[i] = z.randuri[i];
+			this->nrRanduri = z.nrRanduri;
+			for (int i = 0; i < z.nrRanduri; i++)
+				this->scaune[i] = new bool[z.nrScaunePeRand];
+			for (int i = 0; i < z.nrRanduri; i++)
+				for (int j = 0; j < z.nrScaunePeRand; j++)
+					this->scaune[i][j] = z.scaune[i][j];
+			this->nrScaunePeRand = z.nrScaunePeRand;
+			this->pret = z.pret;
 		}
 		return *this;
 	}
@@ -120,52 +85,6 @@ public:
 	{
 		if (denumireNoua != "")
 			denumire = denumireNoua;
-	}
-
-	string* getZone()
-	{
-		if (zone != nullptr && nrZone > 0)
-		{
-			string* copy = new string[nrZone];
-			for (int i = 0; i < nrZone; i++)
-				copy[i] = zone[i];
-			return copy;
-		}
-	}
-
-	void setZone(string* zoneNoi, int nr)
-	{
-		if (zoneNoi != nullptr && nr > 0)
-		{
-			if (zone != nullptr)
-				delete[] zone;
-			nrZone = nr;
-			zone = new string[nr];
-			for (int i = 0; i < nr; i++)
-				zone[i] = zoneNoi[i];
-		}
-	}
-
-	int getNrZone()
-	{
-		return nrZone;
-	}
-
-	void setNrZone(int nrZoneNoi)
-	{
-		if (nrZoneNoi != 0)
-			nrZone = nrZoneNoi;
-	}
-
-	int getNrMaxLocuri()
-	{
-		return nrMaxLocuri;
-	}
-
-	void setNrMaxLocuri(int nr)
-	{
-		if (nr != 0)
-			nrMaxLocuri = nr;
 	}
 
 	bool* getRanduri()
@@ -244,6 +163,230 @@ public:
 			nrScaunePeRand = nr;
 	}
 
+	float getPret()
+	{
+		return pret;
+	}
+
+	void setPret(float pretNou)
+	{
+		if (pretNou > 0)
+			pret = pretNou;
+	}
+
+	string lungimeRanduri(Zona e)
+	{
+		string lungime;
+		if (e.nrScaunePeRand > 10)
+			lungime = "Randurile au fiecare mai mult de 10 locuri";
+		else lungime = "Randurile au cate maxim 10 locuri";
+		return lungime;
+	}
+
+	bool randOcupat(Zona e, int i)
+	{
+		bool ocupat;
+		if (e.randuri[i] = 1)
+			ocupat = 1;
+		else ocupat = 0;
+
+	}
+
+	Zona operator++()
+	{
+		nrRanduri++;
+		return *this;
+	}
+
+	bool operator==(int nr)
+	{
+		if (nrScaunePeRand == nr)
+			return 1;
+		else return 0;
+	}
+
+	friend ostream& operator<<(ostream&, Zona);
+	friend istream& operator>>(istream&, Zona&);
+	friend class Locatie;
+};
+
+ostream& operator<<(ostream& out, Zona z)
+{
+	out << "Denumire: " << z.denumire << endl;
+	out << "Nr. randuri: " << z.nrRanduri;
+	for (int i = 0; i < z.nrRanduri; i++)
+		if (z.randuri[i] = 1)
+			out << "Randul " << i+1 << " este ocupat" << endl;
+		else out << "Randul " << i+1 << " nu este ocupat" << endl;
+	out << "Nr. scaune pe rand: " << z.nrScaunePeRand << endl;
+	for (int i = 0; i < z.nrRanduri; i++)
+		for (int j = 0; j < z.nrScaunePeRand; j++)
+			if (z.scaune[i][j] = 1)
+				out << "Scaunul " << j + 1 << " de pe randul " << i + 1 << " este ocupat" << endl;
+			else out << "Scaunul " << j + 1 << " de pe randul " << i + 1 << " nu este ocupat" << endl;
+	out << "Pret: " << z.pret << endl;
+
+	return out;
+}
+
+istream& operator>>(istream& in, Zona& z)
+{
+	cout << "Denumire: "; in >> ws; getline(in, z.denumire);
+	cout << "Nr. randuri: " << z.nrRanduri;
+	cout << "Randuri: " << endl;
+	if (z.randuri != nullptr)
+	{
+		delete[] z.randuri;
+		z.randuri = nullptr;
+	}
+	z.randuri = new bool[z.nrRanduri];
+	cout << "Introdu 1 daca randul este ocupat sau 0 daca randul nu este ocupat: " << endl;
+	for (int i = 0; i < z.nrRanduri; i++)
+	{
+		cout << "Randul " << i+1 << ": ";
+		in >> z.randuri[i];
+	}
+	cout << endl;
+	cout << "Nr. scaune pe rand: "; in >> z.nrScaunePeRand;
+	cout << "Introdu 1 daca scaunul este ocupat sau 0 daca scaunul nu este ocupat: " << endl;
+	for(int i = 0; i < z.nrRanduri; i++)
+		for (int j = 0; j < z.nrScaunePeRand; j++)
+		{
+			cout << "Scaunul " << j+1 << " de pe randul " << i+1 << ": ";
+			in >> z.scaune[i][j];
+			while (z.scaune[i][j] != 1 && z.scaune[i][j] != 0)
+				in >> z.scaune[i][j];
+		}
+	cout << "Pret: "; in >> z.pret;
+
+	return in;
+}
+
+
+class Locatie {
+private:
+	string denumire;
+	Zona* zone;
+	int nrMaxLocuri;
+	int nrZone;
+	string adresa;
+	string localitate;
+	static string tara;
+
+public:
+	Locatie()
+	{
+		denumire = "Necunoscuta";
+		zone = nullptr;
+		nrZone = 0;
+		nrMaxLocuri = 0;
+		adresa = "Necunoscuta";
+		localitate = "Necunoscuta";
+	}
+
+	Locatie(string denumire, Zona* zone, int nrZone, int nrMaxLocuri, int* randuri, int nrRanduri, int** scaune, int nrScaunePeRand, string adresa, string localitate)
+	{
+		this->denumire = denumire;
+		this->zone = new Zona[nrZone];
+		for (int i = 0; i < nrZone; i++)
+			this->zone[i] = zone[i];
+		this->nrZone = nrZone;
+		this->nrMaxLocuri = nrMaxLocuri;
+		this->adresa = adresa;
+		this->localitate = localitate;
+	}
+
+	~Locatie()
+	{
+		if (nrZone > 0)
+			delete[] zone;
+	}
+
+	Locatie(const Locatie& l)
+	{
+		this->denumire = l.denumire;
+		this->zone = new Zona[l.nrZone];
+		for (int i = 0; i < l.nrZone; i++)
+			this->zone[i] = l.zone[i];
+		this->nrZone = l.nrZone;
+		this->nrMaxLocuri = l.nrMaxLocuri;
+		this->adresa = l.adresa;
+		this->localitate = l.localitate;
+	}
+
+	Locatie operator=(Locatie& l)
+	{
+		if (this != &l)
+		{
+			this->denumire = l.denumire;
+			this->zone = new Zona[l.nrZone];
+			for (int i = 0; i < l.nrZone; i++)
+				this->zone[i] = l.zone[i];
+			this->nrZone = l.nrZone;
+			this->nrMaxLocuri = l.nrMaxLocuri;
+			this->adresa = l.adresa;
+			this->localitate = l.localitate;
+		}
+		return *this;
+	}
+
+	string getDenumire()
+	{
+		return denumire;
+	}
+
+	void setDenumire(string denumireNoua)
+	{
+		if (denumireNoua != "")
+			denumire = denumireNoua;
+	}
+
+	Zona* getZone()
+	{
+		if (zone != nullptr && nrZone > 0)
+		{
+			Zona* copy = new Zona[nrZone];
+			for (int i = 0; i < nrZone; i++)
+				copy[i] = zone[i];
+			return copy;
+		}
+	}
+
+	void setZone(Zona* zoneNoi, int nr)
+	{
+		if (zoneNoi != nullptr && nr > 0)
+		{
+			if (zone != nullptr)
+				delete[] zone;
+			nrZone = nr;
+			zone = new Zona[nr];
+			for (int i = 0; i < nr; i++)
+				zone[i] = zoneNoi[i];
+		}
+	}
+
+	int getNrZone()
+	{
+		return nrZone;
+	}
+
+	void setNrZone(int nrZoneNoi)
+	{
+		if (nrZoneNoi != 0)
+			nrZone = nrZoneNoi;
+	}
+
+	int getNrMaxLocuri()
+	{
+		return nrMaxLocuri;
+	}
+
+	void setNrMaxLocuri(int nr)
+	{
+		if (nr != 0)
+			nrMaxLocuri = nr;
+	}
+
 	string getAdresa()
 	{
 		return adresa;
@@ -307,33 +450,36 @@ public:
 
 	friend ostream& operator<<(ostream&, Locatie);
 	friend istream& operator>>(istream&, Locatie&);
+	friend class Eveniment;
 };
 string Locatie::tara = "Romania";
 
 ostream& operator<<(ostream& out, Locatie l)
 {
 	out << "Denumire: " << l.denumire << endl;
-	out << "Nr. zone: " << l.nrZone << endl;
-	out << "zone: ";
-	for (int i = 0; i < l.nrZone; i++)
-		out << l.zone[i] << " ";
-	out << endl;
 	out << "Nr. maxim de locuri: " << l.nrMaxLocuri << endl;
-	out << "Nr. randuri: " << l.nrRanduri;
-	for (int i = 0; i < l.nrRanduri; i++)
-		out << l.randuri[i] << " ";
-	out << endl;
-	out << "Nr. scaune pe rand: " << l.nrScaunePeRand << endl;
 	out << "Adresa " << l.adresa << endl;
 	out << "Localitate: " << l.localitate << endl;
 	out << "Tara: " << l.getTara << endl;
+	out << "Nr. zone: " << l.nrZone << endl;
+	out << "Zone: ";
+	for (int i = 0; i < l.nrZone; i++)
+	{
+		out << "Detalii despre zona " << i << ": ";
+		ostream& operator<<(ostream& out, Zona z);
+	}
+
 
 	return out;
 }
 
 istream& operator>>(istream& in, Locatie& l)
 {
-	cout << "Denumire: "; in >> l.denumire;
+	cout << "Denumire: "; in >> ws; getline(in, l.denumire);
+	cout << "Nr. maxim de locuri: "; in >> l.nrMaxLocuri;
+	cout << "Adresa: "; in >> ws; getline(in, l.adresa);
+	cout << "Localitate: "; in >> ws; getline(in, l.localitate);
+	cout << "Tara: "; in >> ws; getline(in, l.tara);
 	cout << "Nr. zone: ";  in >> l.nrZone;
 	cout << "zone: ";
 	if (l.zone != nullptr)
@@ -341,63 +487,56 @@ istream& operator>>(istream& in, Locatie& l)
 		delete[] l.zone;
 		l.zone = nullptr;
 	}
-	l.zone = new string[l.nrZone];
+	l.zone = new Zona[l.nrZone];
 	for (int i = 0; i < l.nrZone; i++)
-		in >> l.zone[i];
-	cout << endl;
-	cout << "Nr. maxim de locuri: "; in >> l.nrMaxLocuri;
-	cout << "Randuri: ";
-	cout << "Nr. randuri: " << l.nrRanduri;
-	if (l.randuri != nullptr)
 	{
-		delete[] l.randuri;
-		l.randuri = nullptr;
+		cout << "Zona " << i << ": " << endl;
+		in >> l.zone[i];
 	}
-	l.randuri = new bool[l.nrRanduri];
-	for (int i = 0; i < l.nrRanduri; i++)
-		in >> l.randuri[i];
-	cout << endl;
-	cout << "Nr. scaune pe rand: "; in >> l.nrScaunePeRand;
-	cout << "Adresa: "; in >> l.adresa;
-	cout << "Localitate: "; in >> l.localitate;
-	cout << "Tara: "; in >> l.tara;
 
 	return in;
 }
 
 
+
 class Eveniment {
 private:
-	char* denumire;
+	string denumire;
+	char* domeniuEveniment;
 	string data;
 	int ora;
+	Locatie locatieEveniment;
 
 public:
 	Eveniment()
 	{
-		denumire = nullptr;
+		denumire = "Necunoscuta";
+		domeniuEveniment = nullptr;
 		data = "Necunoscuta";
 		ora = 0;
 	}
 
-	Eveniment(char* denumire, string data, int ora)
+	Eveniment(string denumire, char* domeniuEveniment, string data, int ora)
 	{
 		this->denumire = denumire;
+		this->domeniuEveniment = domeniuEveniment;
 		this->data = data;
 		this->ora = ora;
 	}
 
 	~Eveniment()
 	{
-		if (denumire != nullptr)
-			delete[] denumire;
+		if (domeniuEveniment != nullptr)
+			delete[] domeniuEveniment;
 	}
 
-	Eveniment(const Eveniment& e)
+	Eveniment(Eveniment& e)
 	{
 		this->denumire = e.denumire;
+		this->domeniuEveniment = e.domeniuEveniment;
 		this->data = e.data;
 		this->ora = e.ora;
+		this->locatieEveniment = e.locatieEveniment;
 	}
 
 	Eveniment operator=(Eveniment& e)
@@ -405,32 +544,45 @@ public:
 		if (this != &e)
 		{
 			this->denumire = e.denumire;
+			this->domeniuEveniment = e.domeniuEveniment;
 			this->data = e.data;
 			this->ora = e.ora;
+			this->locatieEveniment = e.locatieEveniment;
 		}
 		return *this;
 	}
 
-	char* getDenumire()
+	string getDenumire()
 	{
-		int n = strlen(denumire);
+		return denumire;
+	}
+
+	void setDenumire(string denumireNoua)
+	{
+		if (denumireNoua != "")
+			denumire = denumireNoua;
+	}
+
+	char* getDomeniuEveniment()
+	{
+		int n = strlen(domeniuEveniment);
 		if (n > 0)
 		{
 			char* copy = new char[n + 1];
-			strcpy_s(copy, n + 1, denumire);
+			strcpy_s(copy, n + 1, domeniuEveniment);
 			return copy;
 		}
 		return NULL;
 	}
 
-	void setDenumire(const char* denumireNoua)
+	void setdomeniuEveniment(const char* domeniuNou)
 	{
-		int n = strlen(denumireNoua);
+		int n = strlen(domeniuNou);
 		if (n > 0)
 		{
-			delete[] denumire;
+			delete[] domeniuEveniment;
 			denumire = new char[n + 1];
-			strcpy_s(denumire, n + 1, denumireNoua);
+			strcpy_s(domeniuEveniment, n + 1, domeniuNou);
 		}
 	}
 
@@ -456,6 +608,18 @@ public:
 			ora = oraNoua;
 	}
 
+	Locatie getLocatieEveniment()
+	{
+		return locatieEveniment;
+	}
+
+	void setLocatieEveniment(Locatie l)
+	{
+		if (l.denumire != "")
+			locatieEveniment = l;
+
+	}
+
 	string programariEvenimente()
 	{
 		string perioada;
@@ -474,8 +638,8 @@ public:
 
 	char& operator[](int index)
 	{
-		if (index >= 0 && denumire != nullptr && index < strlen(denumire))
-			return denumire[index];
+		if (index >= 0 && domeniuEveniment != nullptr && index < strlen(domeniuEveniment))
+			return domeniuEveniment[index];
 	}
 
 	bool operator<(int oraFixa)
@@ -485,29 +649,45 @@ public:
 		else return 0;
 	}
 
+	virtual string accesServicii(int donatie)
+	{
+		string serviciu;
+		if (donatie > 50)
+			serviciu = "Degustare vin";
+		return serviciu;
+	}
+
 	friend ostream& operator<<(ostream&, Eveniment);
 	friend istream& operator>>(istream&, Eveniment&);
+	friend ifstream& operator>>(ifstream&, Eveniment&);
+	friend class Bilet;
 };
 
 ostream& operator<< (ostream& out, Eveniment e)
 {
 	out << "Denumire: " << e.denumire << endl;
+	out << "Domeniul din care face parte evenimentul: " << e.domeniuEveniment << endl;
 	out << "Data: " << e.data << endl;
 	out << "Ora: " << e.ora << endl;
+	out << "Detalii despre locatie: ";
+	ostream& operator<< (ostream & out, Locatie l);
 	return out;
 }
 
 istream& operator>> (istream& in, Eveniment& e)
 {
-	if (e.denumire != nullptr)
+	cout << "Denumire: "; in >> ws; getline(in, e.denumire);
+	if (e.domeniuEveniment != nullptr)
 	{
-		delete[] e.denumire;
-		e.denumire = nullptr;
+		delete[] e.domeniuEveniment;
+		e.domeniuEveniment = nullptr;
 	}
-	e.denumire = new char[100];
-	cout << "Denumire: "; in >> e.denumire;
+	e.domeniuEveniment = new char[100];
+	cout << "Domeniul din care face parte evenimentul: "; in >> e.domeniuEveniment;
 	cout << "Data: "; in >> e.data;
-	cout << "Ora: "; in >> e.ora;
+	cout << "Ora: "; in >> e.ora; cout << endl;
+	cout << "Detalii despre locatie: " << endl;
+	in >> e.locatieEveniment;
 	return in;
 }
 
@@ -515,7 +695,7 @@ istream& operator>> (istream& in, Eveniment& e)
 class Bilet {
 private:
 	const int id;
-	float pret;
+	int pret;
 
 public:
 	Bilet() : id(id)
@@ -583,6 +763,7 @@ public:
 
 	friend ostream& operator<<(ostream&, Bilet);
 	friend istream& operator>>(istream&, Bilet&);
+	friend class Tombola;
 };
 
 ostream& operator<<(ostream& out, Bilet b)
@@ -598,7 +779,59 @@ istream& operator>>(istream& in, Bilet& b)
 	return in;
 }
 
-int main()
+class ActivitateExtra : public Eveniment
 {
 
+public:
+	string accesServicii(int donatie)
+	{
+		string serviciu;
+		if (donatie < 100)
+			serviciu = "Acces doar la bauturi";
+		else serviciu = "Acces atat la bauturi, cat si la mancare";
+		return serviciu;
+	}
+};
+
+class Tombola
+{
+private:
+	Bilet b;
+
+public:
+	virtual string premiiPosibile(unsigned taxa) = 0;
+	virtual bool sansaMaiMare(unsigned taxaNoua)
+	{
+		if (taxaNoua > 150)
+			return 1;
+		else return 0;
+	}
+
+};
+
+class Castig : public Tombola
+{
+public:
+	string premiiPosibile(unsigned taxa)
+	{
+		string premii;
+		if (taxa < 40)
+			premii = "plusuri";
+		else if (taxa < 75)
+			premii = "plusuri, carti";
+		else premii = "plusuri, carti, casti audio";
+		return premii;
+	}
+
+	bool sansaMaiMare(unsigned taxa_Noua)
+	{
+		if (taxa_Noua < 100)
+			return 0;
+		else return 1;
+	}
+};
+
+int main()
+{
+	
 }
